70057556d9bde7353e4d8451702cae2f06556f80
David Duran <dadch1404@gmail.com>
2023-06-21 02:18:04 -0500

    fix: :sparkles: load splitbee only in production

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Router, Route } from 'react-router-dom';
 import { createBrowserHistory } from 'history'; 
 import HomePage from './HomePage';
@@ -11,18 +11,29 @@ const history = createBrowserHistory({
   basename: process.env.REACT_APP_BASE_URL,
 });
 
-const App = ({ feedsStore }) => (
-  <div className='App'>
-    <Router history={history}>
-      <TopBar />
-      <Route path='/' exact>
-        <HomePage feedsStore={feedsStore} />
-      </Route>
-      <Route path='/sources' exact>
-        <SourcesPage feedsStore={feedsStore} />
-      </Route>
-    </Router>
-  </div>
-);
+const App = ({ feedsStore }) => {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      const script = document.createElement("script");
+      script.src = "https://cdn.splitbee.io/sb.js";
+      script.defer = true;
+      document.body.appendChild(script);
+    }
+  }, []);
+
+  return (
+    <div className='App'>
+      <Router history={history}>
+        <TopBar />
+        <Route path='/' exact>
+          <HomePage feedsStore={feedsStore} />
+        </Route>
+        <Route path='/sources' exact>
+          <SourcesPage feedsStore={feedsStore} />
+        </Route>
+      </Router>
+    </div>
+  )
+};
 
 export default App;
